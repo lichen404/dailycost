67bfb42558043736b7b264432e5202b1953a8525
lichen404 <f360family@gmail.com>
2020-01-28 17:17:20 +0800

    全局组件<Nav/>

---
+++ src/views/Labels.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Labels.vue
+        <Nav/>
+    </div>
+</template>
+
+<script lang="ts">
+   import Nav from '@/components/Nav.vue'
+
+    export default {
+        name:'Statistics',
+        components:{Nav}
+        
+    }
+</script>
+
+<style lang="scss scoped">
+
+</style>
