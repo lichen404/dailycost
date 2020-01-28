f542e45b4fc809fab3b28c754c90b141f2c77239
lichen404 <f360family@gmail.com>
2020-01-28 17:25:02 +0800

    增加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
-
+import Notfound from '@/views/Notfound.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,6 +23,10 @@ const routes = [
   {
     path:'/statistics',
     component:Statistics
+  },
+  {
+    path:'*',
+    component:Notfound
   }
 ]
 
