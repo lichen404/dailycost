e0dc2273cfb3e8530e5813cd3629de3e3f264d76
lichen404 <f360family@gmail.com>
2020-02-15 21:40:23 +0800

    使用localStorage和.sync

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,11 +5,11 @@
         <layout class-prefix="xxx">
 
 
-            <number-panel/>
-            <types/>
-            <notes/>
-            <tags :data-source.sync="tags"/>
-
+            <number-panel :value.sync="record.amount" @submit="saveRecord"/>
+            <types :value.sync="record.type"/>
+            <notes :value.sync="record.notes"/>
+            <tags :data-source.sync="tags"  :value.sync="record.tags"/>
+            {{recordList}}
 
         </layout>
     </div>
@@ -18,19 +18,55 @@
 </template>
 
 <script lang="ts">
+    import Vue from 'vue';
     import Tags from '@/components/Money/Tags.vue'
     import Types from "@/components/Money/Types.vue";
     import NumberPanel from "@/components/Money/NumberPanel.vue";
     import Notes from "@/components/Money/Notes.vue";
-
-    export default {
-        name: 'Money',
-        components: {Tags, Notes, NumberPanel, Types},
-        data(){
-            return{
-                tags:['衣','食']
-            }
+    import {Component, Watch} from "vue-property-decorator";
+    // const version = window.localStorage.getItem('version')||0;
+     const recordList:Record[] = JSON.parse(window.localStorage.getItem('recordList')||'[]');
+    //
+    //     if(version==='0.0.1'){
+    //         //数据迁移
+    //         recordList.forEach(record=>{
+    //             record.createAt = new Date(0);
+    //         });
+    //         window.localStorage.setItem('recordList',JSON.stringify(recordList));
+    //     }
+    //
+    // window.localStorage.setItem('version','0.0.2');
+    type Record = {
+        tags: string[]
+        notes: string
+        type: string
+        amount: number // 数据类型
+        createAt?:Date  // 类/构造函数
+    }
+    @Component({
+        components: {
+            Tags, Notes, Types, NumberPanel
         }
+    })
+    export default class Money extends Vue {
+        tags = ['衣', '食', '住', '行'];
+        record: Record = {
+            tags: [], notes: '', type: '-', amount: 0
+        };
+        recordList:Record[]=recordList;
+        saveRecord(){
+            const record2:Record= JSON.parse(JSON.stringify(this.record));
+            record2.createAt = new Date();
+            this.recordList.push(record2);
+
+        }
+        @Watch('recordList')
+        onRecordListChange(){
+            window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+        }
+
+
+
     }
 </script>
 <style lang="scss">
