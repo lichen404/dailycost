65d9afeca14fc26c99d19e17c10b22e4d92513f4
lichen404 <f360family@gmail.com>
2020-03-15 20:40:20 +0800

    修复currentRecord获取错误的问题

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -40,7 +40,7 @@
         type = '-';
 
         get recordList() {
-            return this.$store.state.recordList.filter((i:RecordItem)=>i.type===this.type);
+            return this.$store.state. recordList.filter((r:RecordItem) => r.type === this.type).sort((a,b) => dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf());
         }
 
         currentRecord?: RecordItem;
