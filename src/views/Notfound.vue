4f8305f2a4c5a27f3c46c4c63c2fe6ee334023fb
lichen404 <lichen@genew.com>
2020-09-23 19:52:26 +0800

    修改404页面的样式

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,18 +1,50 @@
 <template>
-    <div>
-        <div>当前页面不存在，请检查网址是否正确</div>
-        <div>
-            <router-link to="/">返回首页</router-link>
-        </div>
+  <div>
+    <div class="wrapper">
+      <Icon name="not-found"/>
+
+      <span class="text">当前页面不存在，请检查网址是否正确</span>
+      <router-link to="/">返回首页</router-link>
     </div>
+
+
+  </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Notfound'
-    }
+import Vue from 'vue';
+import {Component} from "vue-property-decorator";
+import Icon from "@/components/Icon.vue";
+
+@Component({
+  components: {Icon}
+})
+export default class Notfound extends Vue {
+
+
+}
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  > .icon {
+    height: 64px;
+    width: 64px;
+  }
+
+  > .text {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+}
+
+a {
+  text-decoration: underline;
+  color: #0084ff;
+}
 </style>
